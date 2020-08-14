d3eb1716b2d40452eeb1b1c95b65246d200901c5
italosvieira <italo.vieira.dev@gmail.com>
2020-08-13 21:57:29 -0300

    feat: finishing selected credential component

--- src/frontend/components/dashboard/selectedCredential/index.tsx
+++ src/frontend/components/dashboard/selectedCredential/index.tsx
@@ -1,9 +1,9 @@
 import "./index.css";
-import React, { useContext } from "react";
+import React, { CSSProperties, useContext } from "react";
 import { SelectedCredentialContext } from "../../../context/selectedCredential.context";
-import { AmazonOutlined, DownOutlined, WarningOutlined } from "@ant-design/icons/lib";
+import { AmazonOutlined, GoogleOutlined, WarningOutlined } from "@ant-design/icons/lib";
 import { CloudProviderEnum } from "../../../../shared/enums/cloud-provider.enum";
-import { Dropdown, Menu } from "antd";
+import { Select } from "antd";
 import { CredentialsContext } from "../../../context/credentials.context";
 
 interface Props {
@@ -14,63 +14,55 @@ export default function SelectedCredential(props: Props): React.ReactElement {
   const credentialsContext = useContext(CredentialsContext);
   const selectedCredentialContext = useContext(SelectedCredentialContext);
 
-  function noCredential(): React.ReactElement {
-    if (props.isCollapsed) {
-      return <WarningOutlined style={{ fontSize: 24 }} id="collapsed" />;
-    }
-
-    return (
-      <div style={{ paddingLeft: 14 }}>
-        <Dropdown overlay={menu}>
-          <div>
-            <WarningOutlined style={{ fontSize: 24 }} />
-            <span style={{ marginLeft: 5, fontSize: 18 }}>No Credentials</span>
-          </div>
-        </Dropdown>
-      </div>
-    );
-  }
-
-  const menu = (
-    <Menu>
-      {credentialsContext.credentials.map(credential => {
-        return (
-          <Menu.Item key={credential.id} onClick={() => selectedCredentialContext.setCredential(credential)}>
-            <AmazonOutlined />
-            <span>{credential.account}</span>
-          </Menu.Item>
-        );
-      })}
-    </Menu>
-  );
-
-  function getAwsCredential(): React.ReactElement {
-    if (props.isCollapsed) {
-      return <AmazonOutlined style={{ fontSize: 24 }} id="collapsed" />;
-    }
-
-    return (
-      <div style={{ paddingLeft: 14 }}>
-        <Dropdown overlay={menu}>
-          <div>
-            <AmazonOutlined style={{ fontSize: 24 }} />
-            <span style={{ marginLeft: 5, fontSize: 18 }}>{selectedCredentialContext.credential?.account}</span>
-            <DownOutlined style={{ marginLeft: 6, fontSize: 14 }} />
-          </div>
-        </Dropdown>
-      </div>
-    );
-  }
-
-  function getSelectedCredential(): React.ReactElement {
-    if (selectedCredentialContext.credential) {
+  function getCredentialIcon(style: CSSProperties): React.ReactElement {
+    if (selectedCredentialContext?.credential?.cloudProvider) {
       if (selectedCredentialContext.credential.cloudProvider === CloudProviderEnum.AWS) {
-        return getAwsCredential();
+        return <AmazonOutlined style={style} />;
+      }
+
+      if (selectedCredentialContext.credential.cloudProvider === CloudProviderEnum.GCLOUD) {
+        return <GoogleOutlined style={style} />;
       }
     }
 
-    return noCredential();
+    return <WarningOutlined style={style} />;
   }
 
-  return <div className="favorite-credential">{getSelectedCredential()}</div>;
+  function getSelectedCredential(): React.ReactElement {
+    if (props.isCollapsed) {
+      return getCredentialIcon({ fontSize: 24 });
+    }
+
+    return (
+      <div className="selected-credential-left-padding14 selected-credential-right-padding14">
+        <Select
+          value={selectedCredentialContext?.credential?.id}
+          defaultValue={selectedCredentialContext?.credential?.account}
+          className="width100"
+          placeholder={
+            <div>
+              {getCredentialIcon(null)}
+              <span className="selected-credential-left-padding5">No Credentials!</span>
+            </div>
+          }
+          onChange={value => {
+            selectedCredentialContext.setCredential(
+              credentialsContext.credentials.find(credential => credential.id === value),
+            );
+          }}
+        >
+          {credentialsContext.credentials.map(credential => {
+            return (
+              <Select.Option key={credential.id} value={credential.id}>
+                {getCredentialIcon(null)}
+                <span className="selected-credential-left-padding5">{credential.account}</span>
+              </Select.Option>
+            );
+          })}
+        </Select>
+      </div>
+    );
+  }
+
+  return <div className="selected-credential">{getSelectedCredential()}</div>;
 }
